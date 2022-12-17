77aa4c08d556f38c6de82ace2cc6010ac0055d2f
Mateus de Sousa <mateus69107@gmail.com>
2022-12-16 22:20:46 -0300

    :sparckles: feat: formulário de lista

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
-@app.route('/')
+notas = {'Mariana':10, 'Sérgio':5, 'Mateus':9, 'Cláudia':10}
+lista = [] 
+@app.route('/', methods=['GET', 'POST'])
 def main():
-    return render_template('index.html')
+    global lista
+    if request.method == 'POST':
+        if request.form.get("fruta"):
+            lista.append(request.form.get('fruta'))
+    return render_template('index.html', lista = lista)
+
+@app.route('/nota')
+def nota():
+    return render_template('alunos.html', notas = notas)
 
 if '__name__' == ('__main__'):
     app.run(debug = True)
